8ec00c17f4289d4dcc02216f891fae65dde006d5
Elba <benjaminalberto.martinez@alumnos.ulagos.cl>
2023-10-29 17:39:51 -0300

    Se agrega carpeta

---
+++ JAVA/problema1/Maestro.java
@@ -0,0 +1,19 @@
+package problema1;
+import java.util.Scanner;
+public class Maestro {
+    Scanner lec = new Scanner(System.in);
+    String name;
+    int age;
+    String ciudad;
+    Maestro(String name, int age, String ciudad){
+        this.name = name;
+        this.age = age;
+        this.ciudad = ciudad;
+    }
+    void lookName(){
+        System.out.println("El nombre es " + this.name);
+    }
+    void changeName(){
+        this.name = lec.next();
+    }
+}
---
+++ JAVA/problema1/main.java
@@ -0,0 +1,10 @@
+package problema1;
+
+public class main {
+    public static void main (String[] args) {
+        Maestro Jose = new Maestro("Jose",32, "PtoMontt");
+        Jose.lookName();
+        Jose.changeName();
+        Jose.lookName();
+    }
+}
