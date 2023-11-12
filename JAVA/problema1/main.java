3ead98555df006d9c0b835756bc2ee2a62d1a4ab
Elba <benjaminalberto.martinez@alumnos.ulagos.cl>
2023-11-11 21:09:49 -0300

    commit provicional

--- JAVA/problema1/main.java
+++ JAVA/problema1/main.java
@@ -2,7 +2,7 @@ package problema1;
 
 public class main {
     public static void main (String[] args) {
-        Maestro Jose = new Maestro("Jose",32, "PtoMontt");
+        Maestro Jose = new Maestro("Jose", 32, "PuertoMontt");
         Jose.lookName();
         Jose.changeName();
         Jose.lookName();
